023c14d4210b13dccc84630b352a494dd39d213c
tomas.veverka <tomas.veverka@lp-praha.cz>
2020-10-24 12:42:10 +0200

    Initial upload

---
+++ django_login/urls.py
@@ -0,0 +1,16 @@
+"""django_login URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path
+from rest_framework_jwt.views import (
+    obtain_jwt_token,
+    refresh_jwt_token,
+    verify_jwt_token
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(r'api/v1/token-auth/', obtain_jwt_token),
+    path(r'api/v1/token-refresh/', refresh_jwt_token),
+    path(r'api/v1/token-verify/', verify_jwt_token),
+]
